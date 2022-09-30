57cab67dea6519b09949dd446d6c883bbfe541d6
CBovell <venujaperera12@gmail.com>
2022-09-30 01:55:56 -0400

    matrix by matrix multiplication complete

--- matmult.py
+++ matmult.py
@@ -6,7 +6,24 @@ def mult_scalar(matrix, scale):
 	return resMatrix
 
 def mult_matrix(a, b):
-	return [[]]
+	if ((len(b) != len(a[0])) or (len(a) == 0) or (len(b) ==0) or (len(a[0])==0) or (len(b[0]) ==0)):
+		return None
+	
+	resMatrix = [[]*len(a)]
+
+	for i in range(len(b[0])):#For each column in matrix b
+		for j in range (len(a)):#For each row in matrix a
+			currSum=0
+			for k in range (len(a[0])):#For each element in row j
+				currSum+=a[j][k]*b[k][i]#Multiply the element in j row of 'a' by the corrasponding value in i column of 'b' and add it to currSum
+			resMatrix[j].append(currSum)#add currSum to the result Matrix
+	return resMatrix
+			
+
+			
+				
+
+	
 	
 def euclidean_dist(a,b):
 	return -1
